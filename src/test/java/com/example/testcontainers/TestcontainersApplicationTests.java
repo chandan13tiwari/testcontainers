c5caee07f7b91494d019b005fee05ae07067ae04
chandan13tiwari <chandan13tiwaris@gmail.com>
2022-10-16 15:57:37 +0530

    added test container

--- src/test/java/com/example/testcontainers/TestcontainersApplicationTests.java
+++ src/test/java/com/example/testcontainers/TestcontainersApplicationTests.java
@@ -2,20 +2,35 @@ package com.example.testcontainers;
 
 import com.example.testcontainers.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.OutputFrame;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class TestcontainersApplicationTests {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
+
+	@Container
+	private static PostgreSQLContainer container = new PostgreSQLContainer("postgres:latest");
+
+	@DynamicPropertySource
+	public static void overrideProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", container::getJdbcUrl);
+		registry.add("spring.datasource.username", container::getUsername);
+		registry.add("spring.datasource.password", container::getPassword);
+	}
 
 	@Test
 	void successScenarioTest() {
